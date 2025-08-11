b5dd251219b6f0df85640534d9d4892cfd36d9b3
João Victor Roca Moreira <joaovalles@outlook.com>
2025-08-11 11:08:40 -0300

    Adicionar arquivos de projeto.

---
+++ CAD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class login
+{
+    public static void Main(string[] args)
+    {
+        string user, password;
+        
+
+        Console.WriteLine("Usuário");
+        user = Console.ReadLine();
+        Console.WriteLine("Senha");
+        password = Console.ReadLine();
+
+        if (user == "Joao" && password == "281100")
+        {
+            Console.WriteLine("Acesso Permitido");
+        }
+        else
+        {
+            Console.WriteLine("Usuário ou senha inválidos");
+            
+        }
+    }
+}
