a8bf62a91ccfcc25def334f3864a1f0a0c18dc5e
João Victor Roca Moreira <joaovalles@outlook.com>
2025-08-12 09:37:48 -0300

    Update Adicionar

---
+++ CAD/Add.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    internal class Add
+    {
+        public static void AddEntry(string entry)
+        {
+            string product, price, balance;
+
+            Console.WriteLine("Produto");
+            product = Console.ReadLine();
+
+            Console.WriteLine("Preço");
+            price = Console.ReadLine();
+
+            Console.WriteLine("Quantidade");
+            balance = Console.ReadLine();
+            
+
+        }
+
+    }
+}
--- CAD/Program.cs
+++ CAD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Cadastro;
 
 class login
 {
@@ -12,9 +13,39 @@ class login
         Console.WriteLine("Senha");
         password = Console.ReadLine();
 
-        if (user == "Joao" && password == "281100")
+        if (user == "Joao" && password == "1234")
         {
-            Console.WriteLine("Acesso Permitido");
+            Console.WriteLine("Acesso Permitido\n");
+
+            Console.WriteLine("Bem-vindo, " + user + "!\n");
+            Console.WriteLine("Digite a opção que deseja:\n");
+            Console.WriteLine("1 - Cadastrar Produto");
+            Console.WriteLine("2 - Listar Produtos");   
+            Console.WriteLine("3 - Excluir Produto");
+            Console.WriteLine("4 - Sair");
+
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Opção 1 selecionada: Cadastrar Produto\n");
+                    Add.AddEntry("");
+                    break;
+                case "2":
+                    Console.WriteLine("Opção 2 selecionada: Listar Produtos");
+                    // Implementar lógica de listagem de produtos
+                    break;
+                case "3":
+                    Console.WriteLine("Opção 3 selecionada: Excluir Produto");
+                    // Implementar lógica de exclusão de produto
+                    break;
+                case "4":
+                    Console.WriteLine("Saindo do sistema...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
         }
         else
         {
